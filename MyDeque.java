757c2c0ebdb35b24df153ea048deaf43cca3a2a2
will-o-the-wisp <kli18@stuy.edu>
2019-03-21 10:47:39 -0400

    empty methods

---
+++ MyDeque.java
@@ -0,0 +1,27 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+  }
+  public MyDeque(int initialCapacity){
+    }
+  public int size(){
+   }
+  public String toString(){
+   }
+  public void addFirst(E element){
+   }
+  public void addLast(E element){
+   }
+  public E removeFirst(){
+   }
+  public E removeLast(){
+   }
+  public E getFirst(){
+  }
+  public E getLast(){
+   }
+}
