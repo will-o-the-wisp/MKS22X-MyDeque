b9fceef4174d09c4aacd5672d9cd8a14cac23a2c
will-o-the-wisp <kli18@stuy.edu>
2019-03-26 10:39:18 -0400

    wrote and tested convert method

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,26 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
+    public static void main(String[] args){
+      ArrayList<String> a = convert("10 2.0 +");
+      for(int i=0;i<a.size();i++){
+        System.out.println(a.get(i));
+      }
+    }
     public static double eval(String s){
       return 0;
     }
-
+    private static ArrayList<String> convert(String s){
+      ArrayList<String> ans = new ArrayList<String>();
+      int i=0;
+      int j=0;
+      while(i<s.length()+1){
+        if(i==s.length()||s.charAt(i)==' '){
+          ans.add(s.substring(j,i));
+          j=i+1;
+        }
+        i++;
+      }
+      return ans;
+    }
 }
