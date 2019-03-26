58b6d60e48e80a8322b345c30d7366068b50d172
will-o-the-wisp <kli18@stuy.edu>
2019-03-26 10:25:14 -0400

    created skeleton of Calculator

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      return 0;
+    }
+
+}
